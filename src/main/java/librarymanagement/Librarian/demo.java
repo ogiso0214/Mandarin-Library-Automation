a6272eb42f7f7e4f4e8aee9175024e7b68952902
Firstname Lastname <zhaoxueyan1@outlook.com>
2020-03-08 16:51:12 +0800

    Restruct the architecture

--- src/main/java/librarymanagement/Librarian/demo.java
+++ src/main/java/librarymanagement/Librarian/demo.java
@@ -2,15 +2,22 @@ package librarymanagement.Librarian;
 
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.text.AttributedString;
+
+@Controller
 @Component
 public class demo {
 
-    @RequestMapping("/helloWorld")
-    public String first() {
-        return "helloWorld";
+    @RequestMapping("/hello")
+    public String first(@RequestParam(defaultValue = "world") String
+                                    Name, Model model) {
+        model.addAttribute("message", "Hello, "+Name);
+        return "resultPage";
     }
 }
